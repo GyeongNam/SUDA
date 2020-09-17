6d0c65069e4230842851e021318016fa223066c4
dorumamu <64189518+dorumamu@users.noreply.github.com>
2020-09-17 17:29:22 +0900

    signup

--- app/Http/Controllers/Sign_upController.php
+++ app/Http/Controllers/Sign_upController.php
@@ -8,15 +8,13 @@ use Illuminate\Http\Request;
 class Sign_upController extends Controller
 {
 
-    public function signup(Request $request){
-
-      return 0;
-
-      // $user = new user([
-      //   'id' => $request->input('id'),
-      //   'password' => decrypt($request ->input('password')),
-      // 	'phone'=> $request -> input('phone')
-      // ]);
-      // $post->save();
-    }
+  public function signup(Request $request){
+    
+    $user = new user([
+      'id' => $request->input('id'),
+      'password' => decrypt($request ->input('password')),
+      'phone'=> $request -> input('phone')
+    ]);
+    $post->save();
+  }
 }
