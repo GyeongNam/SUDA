85c492b07052c9605fa27562a9b9b9109ec4d1b6
GyeongNam <knightanne97@naver.com>
2020-09-17 18:38:58 +0900

    머지 오류

--- app/Http/Controllers/Sign_upController.php
+++ app/Http/Controllers/Sign_upController.php
@@ -9,11 +9,9 @@ use App\Models\User;
 class Sign_upController extends Controller
 {
 
-<<<<<<< HEAD
     public function signup(Request $request){
 
       return 0;
-
       // $user = new user([
       //   'id' => $request->input('id'),
       //   'password' => decrypt($request ->input('password')),
@@ -35,15 +33,4 @@ class Sign_upController extends Controller
       }
       return response()->json(['data'=> $message]);
     }
-=======
-  public function signup(Request $request){
-    
-    $user = new user([
-      'id' => $request->input('id'),
-      'password' => bcrypt($request ->input('password')),
-      'phone'=> $request -> input('phone')
-    ]);
-    $post->save();
-  }
->>>>>>> 597a87f14c93613f3c70128cfec34e56309139ee
 }
