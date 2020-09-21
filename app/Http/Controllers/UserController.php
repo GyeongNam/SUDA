55162e4d394cc0a7b69580c2b1bf826a84722374
seungji <kwag57@naver.com>
2020-09-22 01:48:36 +0900

    글쓰기 데이터 받기

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,7 +11,7 @@ class UserController extends Controller
     // return $request->userid;
     $userdata = DB::table('users')->where('id',$request->userid)->get();
     // $lastword = substr($userdata,1,-1);
-    $alluserdata = DB::table('users')->get();
+    $alluserdata = DB::table('post')->get();
     // $postlist = json_encode(DB데이터 들어가야함,JSON_PRETTY_PRINT+JSON_UNESCAPED_UNICODE);
     $jsondata = json_encode($alluserdata,JSON_PRETTY_PRINT+JSON_UNESCAPED_UNICODE);//jsondata 쉽게 만들기
 
