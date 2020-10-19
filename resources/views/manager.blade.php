bb521183435353e6b9a31845b59db0df31cb193c
dorumamu <64189518+dorumamu@users.noreply.github.com>
2020-10-19 16:55:22 +0900

    레이아웃 변경 및 수정

--- resources/views/manager.blade.php
+++ resources/views/manager.blade.php
@@ -7,7 +7,6 @@
     <!-- Page Heading -->
     <div class="d-sm-flex align-items-center justify-content-between mb-4">
       <h1 class="h3 mb-0 text-gray-800">SUDA 통계</h1>
-      <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
     </div>
 
     <!-- Content Row -->
@@ -74,7 +73,7 @@
       </div>
 
       <!-- Pending Requests Card Example -->
-      <div class="col-xl-3 col-md-6 mb-4">
+      {{-- <div class="col-xl-3 col-md-6 mb-4">
         <div class="card border-left-warning shadow h-100 py-2">
           <div class="card-body">
             <div class="row no-gutters align-items-center">
@@ -88,7 +87,7 @@
             </div>
           </div>
         </div>
-      </div>
+      </div> --}}
     </div>
 
     <!-- Content Row -->
@@ -102,16 +101,16 @@
           <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
             <h6 class="m-0 font-weight-bold text-primary">게시글 수</h6>
             <div class="dropdown no-arrow">
-              <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              {{-- <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
-              </a>
-              <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in" aria-labelledby="dropdownMenuLink">
+              </a> --}}
+              {{-- <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in" aria-labelledby="dropdownMenuLink">
                 <div class="dropdown-header">Dropdown Header:</div>
                 <a class="dropdown-item" href="#">Action</a>
                 <a class="dropdown-item" href="#">Another action</a>
                 <div class="dropdown-divider"></div>
                 <a class="dropdown-item" href="#">Something else here</a>
-              </div>
+              </div> --}}
             </div>
           </div>
           <!-- Card Body -->
@@ -202,66 +201,7 @@
         <div class="row">
           <div class="col-lg-6 mb-4">
             <div class="card bg-primary text-white shadow">
-              <div class="card-body">
-                Primary
-                <div class="text-white-50 small">#4e73df</div>
-              </div>
-            </div>
-          </div>
-          <div class="col-lg-6 mb-4">
-            <div class="card bg-success text-white shadow">
-              <div class="card-body">
-                Success
-                <div class="text-white-50 small">#1cc88a</div>
-              </div>
-            </div>
-          </div>
-          <div class="col-lg-6 mb-4">
-            <div class="card bg-info text-white shadow">
-              <div class="card-body">
-                Info
-                <div class="text-white-50 small">#36b9cc</div>
-              </div>
-            </div>
-          </div>
-          <div class="col-lg-6 mb-4">
-            <div class="card bg-warning text-white shadow">
-              <div class="card-body">
-                Warning
-                <div class="text-white-50 small">#f6c23e</div>
-              </div>
-            </div>
-          </div>
-          <div class="col-lg-6 mb-4">
-            <div class="card bg-danger text-white shadow">
-              <div class="card-body">
-                Danger
-                <div class="text-white-50 small">#e74a3b</div>
-              </div>
-            </div>
-          </div>
-          <div class="col-lg-6 mb-4">
-            <div class="card bg-secondary text-white shadow">
-              <div class="card-body">
-                Secondary
-                <div class="text-white-50 small">#858796</div>
-              </div>
-            </div>
-          </div>
-          <div class="col-lg-6 mb-4">
-            <div class="card bg-light text-black shadow">
-              <div class="card-body">
-                Light
-                <div class="text-black-50 small">#f8f9fc</div>
-              </div>
-            </div>
-          </div>
-          <div class="col-lg-6 mb-4">
-            <div class="card bg-dark text-white shadow">
-              <div class="card-body">
-                Dark
-                <div class="text-white-50 small">#5a5c69</div>
-              </div>
+
             </div>
           </div>
         </div>
@@ -285,75 +225,15 @@
         </div>
 
         <!-- Approach -->
-        <div class="card shadow mb-4">
+        <div class="card shadow mb-4"> 
           <div class="card-header py-3">
-            <h6 class="m-0 font-weight-bold text-primary">heize(다혜) - Underwater</h6>
+            <h6 class="m-0 font-weight-bold text-primary">LOCO - 잠이들야</h6>
           </div>
           <div class="card-body">
             <p></p>
             <p class="mb-0">
-              일어나서 폰을 보기가<br>
-              두려워 나 좀 내버려 둬<br>
-              오늘도 얼굴 보기가<br>
-              힘들 것 같아 이해해줘<br>
-              씹는 것도 한두 번야<br>
-              피하는 것도 하루 이틀이야<br>
-              왜 미안해야 하니 내가<br>
-              씹는 게 편하진 않아<br>
-              오히려 맘이 무거워져<br>
-              너흰 가벼운 맘였겠지만<br>
-              내 맘엔 작은 방조차 없어<br>
-              죄책감도 좀 들지만<br>
-              좋은 친구 사이가 될 수 없다면<br>
-              음, 안녕! 다른 여잘 찾아가줘<br>
-              <br>
-              난 지금 잠수 중이야<br>
-              상태 메시지 좀 봐줘<br>
-              까만 물안경을 껴서<br>
-              너네가 보이지 않어<br>
-              이건 내 산소 통이야<br>
-              숨 막히니까 비켜 줘<br>
-              난 지금 잠수 중이야<br>
-              난 지금 잠수 중이야<br>
-              <br>
-              내가 싸가지 없다 했다매<br>
-              인마 난 싼마이<br>
-              희망고문 같은 건 안 해<br>
-              술 마시고 이 늦은 밤에<br>
-              전화하는 자넨<br>
-              퍽이나 싸가지가 있는 건가 베?<br>
-              진짜 몰라서 묻는 건지<br>
-              "왜 이렇게 씹냐"고 또 말을 걸지<br>
-              쌓이는 문자들은 내겐 먼지<br>
-              모두 잘 못 찾은<br>
-              번지 난 물속으로 번지<br>
-              why can't you understand me<br>
-              몇 번을 설명했니<br>
-              내 맘속엔 아직<br>
-              한 사람만이 살고 있으니<br>
-              i don't wanna waste my time<br>
-              너네가 싫어서가 아니야<br>
-              오빠가 못나서가 아니에요<br>
-              딴 사람여서 안되는 거예요<br>
-              <br>
-              난 지금 잠수 중이야<br>
-              상태 메시지 좀 봐줘<br>
-              까만 물안경을 껴서<br>
-              너네가 보이지 않어<br>
-              이건 내 산소 통이야<br>
-              숨 막히니까 비켜 줘<br>
-              난 지금 잠수 중이야<br>
-              난 지금 잠수 중이야<br>
-              <br>
-              풍 덩 빠지고 싶어<br>
-              너희들이 없는 바다로<br>
-              풍 덩 빠지고 싶어<br>
-              너희들이 없는 바다로<br>
-              풍 덩 빠지고 싶어<br>
-              너희들이 없는 바다로<br>
-              풍 덩 빠지고 싶어<br>
-              <br>
-              나 다시 그대의 품 속으로<br>
+              잠이들어야<br>
+              내일이올텐데<br>
             </p>
           </div>
         </div>
