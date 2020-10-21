973e5a3ca4e01c8e3aee0149aabf77e626922a03
dorumamu <64189518+dorumamu@users.noreply.github.com>
2020-10-21 14:48:59 +0900

    feat.조경남. 조승지

--- public/template/js/demo/chart-bar-demo.js
+++ public/template/js/demo/chart-bar-demo.js
@@ -26,19 +26,18 @@ function number_format(number, decimals, dec_point, thousands_sep) {
   }
   return s.join(dec);
 }
-
 // Bar Chart Example
 var ctx = document.getElementById("myBarChart");
 var myBarChart = new Chart(ctx, {
   type: 'bar',
   data: {
-    labels: ["January", "February", "March", "April", "May", "June"],
+    labels: array,
     datasets: [{
-      label: "Revenue",
+      label: "방문자 수 ",
       backgroundColor: "#4e73df",
       hoverBackgroundColor: "#2e59d9",
       borderColor: "#4e73df",
-      data: [4215, 5312, 6251, 7841, 9821, 14984],
+      data: array1,
     }],
   },
   options: {
@@ -68,12 +67,12 @@ var myBarChart = new Chart(ctx, {
       yAxes: [{
         ticks: {
           min: 0,
-          max: 15000,
+          max: 100,
           maxTicksLimit: 5,
           padding: 10,
           // Include a dollar sign in the ticks
           callback: function(value, index, values) {
-            return '$' + number_format(value);
+            return number_format(value);
           }
         },
         gridLines: {
@@ -103,7 +102,89 @@ var myBarChart = new Chart(ctx, {
       callbacks: {
         label: function(tooltipItem, chart) {
           var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
-          return datasetLabel + ': $' + number_format(tooltipItem.yLabel);
+          return datasetLabel + ': ' + number_format(tooltipItem.yLabel);
+        }
+      }
+    },
+  }
+});
+
+var ctx = document.getElementById("myBarChartM");
+var myBarChartM = new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: arrayM,
+    datasets: [{
+      label: "방문자 수 ",
+      backgroundColor: "#4e73df",
+      hoverBackgroundColor: "#2e59d9",
+      borderColor: "#4e73df",
+      data: arrayM1,
+    }],
+  },
+  options: {
+    maintainAspectRatio: false,
+    layout: {
+      padding: {
+        left: 10,
+        right: 25,
+        top: 25,
+        bottom: 0
+      }
+    },
+    scales: {
+      xAxes: [{
+        time: {
+          unit: 'month'
+        },
+        gridLines: {
+          display: false,
+          drawBorder: false
+        },
+        ticks: {
+          maxTicksLimit: 6
+        },
+        maxBarThickness: 25,
+      }],
+      yAxes: [{
+        ticks: {
+          min: 0,
+          max: 100,
+          maxTicksLimit: 5,
+          padding: 10,
+          // Include a dollar sign in the ticks
+          callback: function(value, index, values) {
+            return number_format(value);
+          }
+        },
+        gridLines: {
+          color: "rgb(234, 236, 244)",
+          zeroLineColor: "rgb(234, 236, 244)",
+          drawBorder: false,
+          borderDash: [2],
+          zeroLineBorderDash: [2]
+        }
+      }],
+    },
+    legend: {
+      display: false
+    },
+    tooltips: {
+      titleMarginBottom: 10,
+      titleFontColor: '#6e707e',
+      titleFontSize: 14,
+      backgroundColor: "rgb(255,255,255)",
+      bodyFontColor: "#858796",
+      borderColor: '#dddfeb',
+      borderWidth: 1,
+      xPadding: 15,
+      yPadding: 15,
+      displayColors: false,
+      caretPadding: 10,
+      callbacks: {
+        label: function(tooltipItem, chart) {
+          var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
+          return datasetLabel + ': ' + number_format(tooltipItem.yLabel);
         }
       }
     },
--- resources/views/manager.blade.php
+++ resources/views/manager.blade.php
@@ -11,32 +11,6 @@
 
     <!-- Content Row -->
     <div class="row">
-    </div>
-
-    <!-- Content Row -->
-
-    <div class="row">
-
-      <!-- Area Chart -->
-      <div class="col-xl-8 col-lg-7">
-        <div class="card shadow mb-4">
-          <!-- Card Header - Dropdown -->
-          <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-            <h6 class="m-0 font-weight-bold text-primary">게시글 수</h6>
-            <div class="dropdown no-arrow">
-              {{-- <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
-            </a> --}}
-            {{-- <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in" aria-labelledby="dropdownMenuLink">
-            <div class="dropdown-header">Dropdown Header:</div>
-            <a class="dropdown-item" href="#">Action</a>
-            <a class="dropdown-item" href="#">Another action</a>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="#">Something else here</a>
-          </div> --}}
-        </div>
-      </div>
-      <!-- Card Body -->
       <div class="col-xl-3 col-md-6 mb-4">
         <div class="card border-left-primary shadow h-100 py-2">
           <div class="card-body">
@@ -72,7 +46,22 @@
           <div class="card-body">
             <div class="row no-gutters align-items-center">
               <div class="col mr-2">
-                <div class="text-xs font-weight-bold text-success text-uppercase mb-1">이번달 발생한 오류</div>
+                <div class="text-xs font-weight-bold text-success text-uppercase mb-1">월 방문자 수</div>
+                <div class="h5 mb-0 font-weight-bold text-gray-800">{{$month_visitors_count}}개</div>
+              </div>
+              <div class="col-auto">
+                <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-xl-3 col-md-6 mb-4">
+        <div class="card border-left-success shadow h-100 py-2">
+          <div class="card-body">
+            <div class="row no-gutters align-items-center">
+              <div class="col mr-2">
+                <div class="text-xs font-weight-bold text-success text-uppercase mb-1">월 발생한 오류</div>
                 <div class="h5 mb-0 font-weight-bold text-gray-800">{{$month_error_count}}개</div>
               </div>
               <div class="col-auto">
@@ -83,27 +72,12 @@
         </div>
       </div>
     </div>
-    <div class="card shadow mb-4">
-      <!-- Card Header - Dropdown -->
-      <div class="card-header py-3">
-        <h6 class="m-0 font-weight-bold text-primary">게시판별 게시물 현황</h6>
-      </div>
-      <!-- Card Body -->
-      <div class="card-body">
-        <div class="chart-pie pt-4">
-          <canvas id="myPieChart"></canvas>
-        </div>
-        <hr>
-        게시판별 게시물 현황을 파이형식 차트로 나타낸 부분 <code>/js/demo/chart-pie-demo.js</code> file.
-      </div>
-    </div>
-    <input type="hidden" name="pie_key" value="{{}}">
   </div>
 </div>
 <!-- Bar Chart -->
 <div class="card shadow mb-4">
   <div class="card-header py-3">
-    <h6 class="m-0 font-weight-bold text-primary">Bar Chart</h6>
+    <h6 class="m-0 font-weight-bold text-primary">게시판별 방문자 수(어제 기준)</h6>
   </div>
   <div class="card-body">
     <div class="chart-bar">
@@ -113,5 +87,53 @@
     Styling for the bar chart can be found in the <code>/js/demo/chart-bar-demo.js</code> file.
   </div>
 </div>
+<!-- Bar Chart -->
+<div class="card shadow mb-4">
+  <div class="card-header py-3">
+    <h6 class="m-0 font-weight-bold text-primary">게시판별 방문자 수(월별 기준)</h6>
+  </div>
+  <div class="card-body">
+    <div class="chart-bar">
+      <canvas id="myBarChartM"></canvas>
+    </div>
+    <hr>
+    Styling for the bar chart can be found in the <code>/js/demo/chart-bar-demo.js</code> file.
+  </div>
 </div>
+
+</div>
+@foreach ($yesterday_json_board as $key => $value)
+  <input type="hidden" name="board" value="{{$key}}">
+  <input type="hidden" name="visit" value="{{$value}}">
+
+@endforeach
+
+@foreach ($month_json_board as $key => $value)
+  <input type="hidden" name="Mboard" value="{{$key}}">
+  <input type="hidden" name="Mvisit" value="{{$value}}">
+@endforeach
+
+<script>
+var x = $('input[name=board]').length
+var array =  new Array();
+var array1 = new Array();
+for(i=0;i<x;i++){
+  array.push($('input[name=board]').eq(i).val());
+  array1.push($('input[name=visit]').eq(i).val());
+}
+console.log(array);
+console.log(array1);
+</script>
+
+<script>
+var y = $('input[name=Mboard]').length
+var arrayM =  new Array();
+var arrayM1 = new Array();
+for(i=0;i<y;i++){
+  arrayM.push($('input[name=Mboard]').eq(i).val());
+  arrayM1.push($('input[name=Mvisit]').eq(i).val());
+}
+console.log(arrayM);
+console.log(arrayM1);
+</script>
 @endsection
