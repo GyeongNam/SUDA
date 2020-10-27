6c4e866ebbf322d5d89381752f86e78fd6428bb5
dorumamu <64189518+dorumamu@users.noreply.github.com>
2020-10-27 16:11:45 +0900

    게시글 알람 설정

--- app/Http/Controllers/Listalarm.php
+++ app/Http/Controllers/Listalarm.php
@@ -7,7 +7,15 @@ use Illuminate\Http\Request;
 
 class Listalarm extends Controller
 {
-    public function alarm(Request $request) {
-      
+  public function alarm(Request $request) {
+    $data = DB::table('post_notification')->where('user_id',$request->userinfo)->where('categorie_num',$request->KEY)->get()->count();
+
+    if($data == 1){
+      DB::table('post_notification')->where('user_id',$request->userinfo)->where('categorie_num',$request->KEY)->delete();
     }
+    else{
+      DB::table('post_notification')->insert(['user_id' => $request->userinfo, 'categorie_num' => $request->KEY ]);
+  }
+  return 0;
+ }
 }
